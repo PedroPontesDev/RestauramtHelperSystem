4c1353eb38f8bc301e5dc92c67b0bdcb90832bee
Pedro Pontes <pedrohenriquepsantos2000@gmail.com>
2024-06-01 21:41:05 -0300

    Struct project ,akr im
    8
    ]
    
    ']
    ]]]]]]]]
    
    clear
    exit

---
+++ RestaurantHelper/src/main/java/com/br/devPomtes/RestaurantHelper/RestaurantHelperApplication.java
@@ -0,0 +1,13 @@
+package com.br.devPomtes.RestaurantHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantHelperApplication.class, args);
+	}
+
+}
---
+++ RestaurantHelper/src/test/java/com/br/devPomtes/RestaurantHelper/RestaurantHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.devPomtes.RestaurantHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
